17c7b4b1d680b1a6de73513a4075d16e573df825
Mike Petersen <mike@odania-it.de>
2015-01-26 18:32:39 +0100

    Fixed command calls

--- install.sh
+++ install.sh
@@ -6,13 +6,13 @@ set -x
 minimal_apt_get_install build-essential apache2 php5 libapache2-mod-php5 php5-dev php-pear wget unzip
 
 ## Install mongo driver
-RUN pecl install mongo
-RUN touch /etc/php5/conf.d/mongo.ini
-RUN echo "extension=mongo.so" >> /etc/php5/conf.d/mongo.ini
+pecl install mongo
+touch /etc/php5/conf.d/mongo.ini
+echo "extension=mongo.so" >> /etc/php5/conf.d/mongo.ini
 
 # Install Rockmongo
-RUN cd /root && wget --no-check-certificate https://github.com/gilacode/rockmongo/archive/master.zip -O rockmongo-master.zip
-RUN cd /root && unzip rockmongo-master.zip -d /var/ && rm -fr /var/www && mv /var/rockmongo-master/ /var/www/rockmongo
+cd /root && wget --no-check-certificate https://github.com/gilacode/rockmongo/archive/master.zip -O rockmongo-master.zip
+cd /root && unzip rockmongo-master.zip -d /var/ && rm -fr /var/www && mv /var/rockmongo-master/ /var/www/rockmongo
 
 ## Install haproxy runit service.
 mkdir /etc/service/apache2
